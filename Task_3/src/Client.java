09ed2f1dd7c08f26b5133fc10690663391871fac
Polyakov Nikita <51836485+nik-2@users.noreply.github.com>
2019-08-23 16:01:24 +0300

    Update Client.java

--- Task_3/src/Client.java
+++ Task_3/src/Client.java
@@ -3,34 +3,25 @@ import java.net.Socket;
 
 public class Client {
 
-    private static Socket clientSocket; //сокет для общения
-    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
-    // мы узнаем что хочет сказать клиент?
-    private static BufferedReader in; // поток чтения из сокета
-    private static BufferedWriter out; // поток записи в сокет
+    private static Socket clientSocket; 
+    private static BufferedReader reader; 
+    private static BufferedReader in; 
+    private static BufferedWriter out;
 
     public static void main(String[] args) {
         try {
             try {
-                // адрес - локальный хост, порт - 4004, такой же как у сервера
-                clientSocket = new Socket("localhost", 4004); // этой строкой мы запрашиваем
-                //  у сервера доступ на соединение
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                // читать соообщения с сервера
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                // писать туда же
+                clientSocket = new Socket("localhost", 4004);         
+                reader = new BufferedReader(new InputStreamReader(System.in));            
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));              
                 out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                System.out.println("Вы что-то хотели сказать? Введите это здесь:");
-                // если соединение произошло и потоки успешно созданы - мы можем
-                //  работать дальше и предложить клиенту что то ввести
-                // если нет - вылетит исключение
-                String word = reader.readLine(); // ждём пока клиент что-нибудь
-                // не напишет в консоль
-                out.write(word + "\n"); // отправляем сообщение на сервер
+                System.out.println("Вы что-то хотели сказать? Введите это здесь:");            
+                String word = reader.readLine();              
+                out.write(word + "\n"); 
                 out.flush();
-                String serverWord = in.readLine(); // ждём, что скажет сервер
-                System.out.println(serverWord); // получив - выводим на экран
-            } finally { // в любом случае необходимо закрыть сокет и потоки
+                String serverWord = in.readLine();
+                System.out.println(serverWord); 
+            } finally {
                 System.out.println("Клиент был закрыт...");
                 clientSocket.close();
                 in.close();
